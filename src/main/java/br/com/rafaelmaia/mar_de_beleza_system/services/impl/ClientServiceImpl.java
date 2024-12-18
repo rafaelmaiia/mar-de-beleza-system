46a50ada4021008c2a0f5af3c5adddf63f90ec24
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-18 17:57:37 -0300

    feat: create class UserServiceImpl with findById

---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/services/impl/ClientServiceImpl.java
@@ -0,0 +1,21 @@
+package br.com.rafaelmaia.mar_de_beleza_system.services.impl;
+
+import br.com.rafaelmaia.mar_de_beleza_system.domain.entity.Client;
+import br.com.rafaelmaia.mar_de_beleza_system.repository.ClientRepository;
+import br.com.rafaelmaia.mar_de_beleza_system.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    public Client findClientById(Long id) {
+        Optional<Client> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
