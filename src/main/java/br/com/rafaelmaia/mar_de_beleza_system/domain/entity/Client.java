c5a5485ecc891a5d7a8d423c378d66f18f5e13d4
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-18 17:44:34 -0300

    feat: create Client entity in domain package

---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/entity/Client.java
@@ -0,0 +1,30 @@
+package br.com.rafaelmaia.mar_de_beleza_system.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_client")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+}
