138db324e0fae5bec95c25fbc7a25ccbd374b2f7
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-18 17:31:46 -0300

    feat: create project
    - Imported project to IntelliJ IDEA
    - Added dependencies: Spring Web, Spring Data JPA, PostgreSQL Driver, Validation, Lombok

---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/MarDeBelezaSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmaia.mar_de_beleza_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarDeBelezaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarDeBelezaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelmaia/mar_de_beleza_system/MarDeBelezaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmaia.mar_de_beleza_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarDeBelezaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
