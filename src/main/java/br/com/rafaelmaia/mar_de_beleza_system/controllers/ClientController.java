e9690ed1a5bacdf8639ffface584b631a240bfc1
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-18 18:02:27 -0300

    create controller package and class ClientController with findById method

---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/controllers/ClientController.java
@@ -0,0 +1,22 @@
+package br.com.rafaelmaia.mar_de_beleza_system.controllers;
+
+import br.com.rafaelmaia.mar_de_beleza_system.domain.entity.Client;
+import br.com.rafaelmaia.mar_de_beleza_system.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/client")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> findClientById(@PathVariable Long id) {
+
+        return ResponseEntity.ok().body(service.findClientById(id));
+    }
+}
