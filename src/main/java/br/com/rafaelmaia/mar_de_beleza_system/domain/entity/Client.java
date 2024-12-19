500ffa7c1baf25ddfabcbb48a9b15e0c1510475e
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-18 21:42:49 -0300

    refactor: remove Lombok annotations

--- src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/entity/Client.java
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/entity/Client.java
@@ -1,19 +1,12 @@
 package br.com.rafaelmaia.mar_de_beleza_system.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_client")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Client implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -27,4 +20,27 @@ public class Client implements Serializable {
 
     @Column(unique = true)
     private String phone;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(name, client.name) && Objects.equals(email, client.email) && Objects.equals(phone, client.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, phone);
+    }
 }
