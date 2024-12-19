1384ddd6d2d6ebe81d92b692d94de22e875eb085
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-19 14:26:57 -0300

    refactor: return ClientDTO in ClientController

--- src/main/java/br/com/rafaelmaia/mar_de_beleza_system/controllers/ClientController.java
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/controllers/ClientController.java
@@ -1,7 +1,8 @@
 package br.com.rafaelmaia.mar_de_beleza_system.controllers;
 
-import br.com.rafaelmaia.mar_de_beleza_system.domain.entity.Client;
+import br.com.rafaelmaia.mar_de_beleza_system.dto.ClientDTO;
 import br.com.rafaelmaia.mar_de_beleza_system.services.ClientService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +17,22 @@ import java.util.List;
 public class ClientController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private ClientService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Client> findClientById(@PathVariable Long id) {
+    public ResponseEntity<ClientDTO> findClientById(@PathVariable Long id) {
 
-        return ResponseEntity.ok().body(service.findClientById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findClientById(id), ClientDTO.class));
     }
 
     @GetMapping
-    public ResponseEntity<List<Client>> findAllClients() {
+    public ResponseEntity<List<ClientDTO>> findAllClients() {
 
-        return ResponseEntity.ok().body(service.findAllClients());
+        return ResponseEntity.ok().body(
+                service.findAllClients().stream().map(client -> mapper.map(client, ClientDTO.class)).toList()
+        );
     }
 }
