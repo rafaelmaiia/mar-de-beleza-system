5d4f3f30c08e906ef761cbe8211b6d3f3e2cd486
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-19 14:27:40 -0300

    refactor: add Lombok annotations

--- src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/entity/Client.java
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/entity/Client.java
@@ -1,46 +1,33 @@
 package br.com.rafaelmaia.mar_de_beleza_system.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
 
     @Column(unique = true)
     private String email;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String phone;
-
-    public Client() {
-    }
-
-    public Client(Long id, String name, String email, String phone) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return Objects.equals(id, client.id) && Objects.equals(name, client.name) && Objects.equals(email, client.email) && Objects.equals(phone, client.phone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, phone);
-    }
 }
