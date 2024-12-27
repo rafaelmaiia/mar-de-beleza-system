5c36dfa201bbbd2b786603be475af68e2a8c7b7a
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-12-27 16:34:35 -0300

    feat: add enums for statuses and types

---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/enums/EyebrowType.java
@@ -0,0 +1,11 @@
+package br.com.rafaelmaia.mar_de_beleza_system.domain.enums;
+
+public enum EyebrowType {
+    MICROBLADING,
+    MICROPIGMENTACAO,
+    DESIGN_COM_HENNA,
+    DESIGN_COM_TINTURA,
+    LAMINACAO,
+    BROWLIFT,
+    DESIGN_SIMPLES
+}
---
+++ src/main/java/br/com/rafaelmaia/mar_de_beleza_system/domain/enums/LashType.java
@@ -0,0 +1,11 @@
+package br.com.rafaelmaia.mar_de_beleza_system.domain.enums;
+
+public enum LashType {
+    VOLUME_BRASILEIRO,
+    VOLUME_RUSSO,
+    VOLUME_EGIPCIO,
+    VOLUME_HOLLYWOOD,
+    VOLUME_CLASSICO,
+    FIOS_A_FIOS,
+    NATURAL
+}
