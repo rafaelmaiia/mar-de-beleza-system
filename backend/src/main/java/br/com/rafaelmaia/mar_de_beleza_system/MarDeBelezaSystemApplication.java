cb041937d9ffabdba38b69c2ad989b2e2388ef5b
rafaelmaiia <rafaelmaia3002@gmail.com>
2025-07-30 18:51:09 -0300

    chore(config): enable stable page serialization
    Adds @EnableSpringDataWebSupport annotation to ensure a stable JSON structure for paginated responses, resolving a Spring Data warning.

--- backend/src/main/java/br/com/rafaelmaia/mar_de_beleza_system/MarDeBelezaSystemApplication.java
+++ backend/src/main/java/br/com/rafaelmaia/mar_de_beleza_system/MarDeBelezaSystemApplication.java
@@ -2,8 +2,11 @@ package br.com.rafaelmaia.mar_de_beleza_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class MarDeBelezaSystemApplication {
 
 	public static void main(String[] args) {
